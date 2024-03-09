9cfeb3245068159e36417cc2153dc5b5ab3c8588
Jurupoc <jurupoc222@gmail.com>
2024-03-09 14:37:23 -0300

    Converting At and Set function to Methods

--- api/model/matrix.go
+++ api/model/matrix.go
@@ -18,10 +18,10 @@ func getIndex(matrix *Matrix, row, column int) int {
 	return (row * matrix.Cloumns) + column
 }
 
-func At(matrix *Matrix, row, column int) int {
+func (matrix *Matrix) At(row, column int) int {
 	return matrix.Data[getIndex(matrix, row, column)]
 }
 
-func Set(matrix *Matrix, row, column, value int) {
+func (matrix *Matrix)  Set(row, column, value int) {
 	matrix.Data[getIndex(matrix, row, column)] = value
 }
